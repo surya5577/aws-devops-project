9eb11fd87b3209c9eef019a1d0e0aed783b10a9e
surya5577 <kr1971165@gmail.com>
2026-07-26 09:32:27 +0000

    Initial Spring Boot Project

---
+++ src/main/java/com/devops/aws/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.devops.aws.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Enterprise DevOps Project!";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "Application is Running Successfully!";
+    }
+
+    @GetMapping("/version")
+    public String version() {
+        return "Version 1.0.0";
+    }
+}
